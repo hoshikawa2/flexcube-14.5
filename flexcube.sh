e049aeb71ae61a22cd095097ca839cad608fd77c
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2022-10-11 19:08:40 -0300

    flexcube.sh update

--- flexcube.sh
+++ flexcube.sh
@@ -1,3 +1,5 @@
+yum install unzip -y
+yum install wget -y
 su - gsh
 cd /
 #wget "https://objectstorage.ap-mumbai-1.oraclecloud.com/p/x_jm7V8SukeVvV0Jeh-Pf3u_B_bWtcyn1nZ1CDcwaGLngSi4o-eUtdNtV4joKFK2/n/ids3optpuczi/b/14.5Deployment/o/kernel145_17Nov21.zip"
